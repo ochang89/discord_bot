df2e0ed880e419560244e4ed8c287e347810b6c5
Oliver Chang <=>
2023-03-05 12:25:33 -0500

    bot initialized

---
+++ main.ts
@@ -0,0 +1,9 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+// const Discord = require('discord.js');
+// const client = new Discord.Client();
+
+require('dotenv');
+console.log(process.env);
+
